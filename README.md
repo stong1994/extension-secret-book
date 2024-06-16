27424a3cad096f49ff4717356eb45596dbdbfb83
stong <498327580@qq.com>
2024-06-16 23:50:16 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# ID Extractor
+
+ID Extractor is a web browser plugin designed to extract IDs from web URLs.
+
+## Features
+
+- Extracts multiple IDs from a URL using regular expressions.
+- Supports various ID formats:
+  - UUID: e.g., "20a7fa5d-55c5-4364-ae81-46d4472865ab"
+  - UUID Hex: e.g., "20a7fa5d55c54364ae8146d4472865ab"
+  - MongoDB ID: e.g., "6642ce35337ea0f207e4d7a9"
+- If there are multiple id found, we will use ',' to contact them.
+
+## Notifications
+
+The plugin sends a notification after an ID has been copied. This occurs even if no ID was found. Ensure your browser has the necessary permissions to display these notifications.
+
+## Shortcuts
+
+- Mac users: `Command+B`
+- Windows users: `Ctrl+B`
+
+## Icon
+
+The plugin's icon was generated using [favicon.io](https://favicon.io/favicon-generator/).
---
+++ script.js
@@ -0,0 +1,66 @@
+window.onload = function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var url = new URL(tabs[0].url);
+    var host = url.hostname;
+
+    fetch(
+      `http://192.168.110.168:12345/fetch_state?host=${encodeURIComponent(host)}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        var accountsList = document.getElementById("accounts");
+        accountsList.innerHTML = ""; // Clear the list
+
+        data.forEach((item) => {
+          if (item.content) {
+            var info = item.content.split(" ");
+            var account = info[0];
+            var password = info[1];
+
+            // Create a new list item
+            var li = document.createElement("li");
+
+            // Create a div for the account
+            var accountDiv = createDivWithButton(
+              "Account: " + account,
+              "copy",
+              function () {
+                navigator.clipboard.writeText(account);
+              },
+            );
+            li.appendChild(accountDiv);
+
+            // Create a div for the password
+            var passwordDiv = createDivWithButton(
+              "Password: ********",
+              "copy",
+              function () {
+                navigator.clipboard.writeText(password);
+              },
+            );
+            li.appendChild(passwordDiv);
+
+            // Append the list item to the list
+            accountsList.appendChild(li);
+          }
+        });
+      })
+      .catch((error) => console.log("Error:", error));
+  });
+};
+
+function createDivWithButton(text, buttonText, buttonClickCallback) {
+  var div = document.createElement("div");
+  div.textContent = text;
+
+  var button = document.createElement("button");
+  button.textContent = buttonText;
+  button.addEventListener("click", function () {
+    buttonClickCallback();
+    this.style.backgroundColor = "#FF7F3E"; // Change the color of the button to #536626
+    setTimeout(() => (this.style.backgroundColor = ""), 500); // Change it back after 2 seconds
+  });
+  div.appendChild(button);
+
+  return div;
+}
