273a2b0e4fbe8139f773e6431a1d17376e43d0e2
stong <498327580@qq.com>
2024-06-17 10:14:09 +0800

    feat: Add server ping and notification feature
    - Added a ping to the server before saving the server host
    
    - Added a notification function to notify users about errors
    
    - Added notifications permission in manifest.json

--- config.js
+++ config.js
@@ -1,9 +1,37 @@
-window.onload = function () {
-  document.getElementById("save").addEventListener("click", function () {
-    var serverHost = document.getElementById("serverHost").value;
-    chrome.storage.local.set({ serverHost: serverHost }, function () {
-      console.log("Server host saved");
-      window.close(); // Close the configuration page
+document.getElementById("save").addEventListener("click", function () {
+  var serverHost = document.getElementById("serverHost").value;
+
+  var timeoutPromise = new Promise((_, reject) =>
+    setTimeout(() => reject(new Error("Request timed out")), 1000),
+  );
+
+  Promise.race([
+    fetch(`${serverHost}/ping`).then((response) => response.text()),
+    timeoutPromise,
+  ])
+    .then((data) => {
+      if (data != "PONG") {
+        console.error("Invalid server host, expected got PONG, got: " + data);
+        notify("Error", "Invalid server host");
+      } else {
+        chrome.storage.local.set({ serverHost: serverHost }, function () {
+          console.log("Server host saved");
+          window.close(); // Close the configuration page
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("ping server failed, error: ", error);
+      notify("Error", error.message);
     });
+});
+
+function notify(title, message) {
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "icons/icon32.png", // Replace with your extension's icon
+    title: title,
+    message: message,
+    priority: 1,
   });
-};
+}
