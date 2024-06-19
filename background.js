c5647a0501c46f3e919950ee50fffcc8790b1e5b
stong <498327580@qq.com>
2024-06-19 15:17:17 +0800

    refactor: Improve code readability in background.js
    - Added a condition to check if host is "extensions" and return if true
    
    - Reformatted the Promise.race block for better readability
    
    - Reformatted the chrome.runtime.onMessage.addListener block for better readability

--- background.js
+++ background.js
@@ -7,41 +7,46 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && tab.active && tab.url) {
     var url = new URL(tab.url);
     var host = url.hostname;
+    if (host === "extensions") {
+      return;
+    }
     var timeoutPromise = new Promise((_, reject) =>
       setTimeout(() => reject(new Error("Request timed out")), 1000),
     );
     chrome.storage.local.get(["serverHost"], function (result) {
-       var serverHost = result.serverHost;
-  if (!serverHost) {
-    return;
-    }
-    Promise.race([
-      fetch(`${serverHost}/fetch_state?host=${encodeURIComponent(host)}`).then(
-        (response) => response.json(),
-      ),
-      timeoutPromise,
-    ])
-      .then((account) => {
-        console.log("got accounts", account);
-        // Store account info in local storage
-        chrome.storage.local.set({ [tabId.toString()]: account }, function () {
-          console.log("Account info saved");
-        });
-      })
-      .catch((error) => console.log("Error:", error));
-  });
+      var serverHost = result.serverHost;
+      if (!serverHost) {
+        return;
+      }
+      Promise.race([
+        fetch(
+          `${serverHost}/fetch_state?host=${encodeURIComponent(host)}`,
+        ).then((response) => response.json()),
+        timeoutPromise,
+      ])
+        .then((account) => {
+          console.log("got accounts", account);
+          // Store account info in local storage
+          chrome.storage.local.set(
+            { [tabId.toString()]: account },
+            function () {
+              console.log("Account info saved");
+            },
+          );
+        })
+        .catch((error) => console.log("Error:", error));
+    });
   }
 });
 
 // Listen for tab close
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    // Remove account info from local storage
-    chrome.storage.local.remove([tabId.toString()], function () {
-  });
+  // Remove account info from local storage
+  chrome.storage.local.remove([tabId.toString()], function () {});
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.type === "getTabId") {
-    sendResponse({tabId: sender.tab.id});
+    sendResponse({ tabId: sender.tab.id });
   }
 });
